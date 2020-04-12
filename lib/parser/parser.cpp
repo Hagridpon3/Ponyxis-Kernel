b30279ef899c5f896cf05768a2cab8f2d04d5cb3
ponyxis <tomik@sazava.eu>
2020-04-12 08:57:40 +0200

    Add beep command
    Add beep command: beep [frequency] [duration] adn set version to 0.9.0.0

--- lib/parser/parser.cpp
+++ lib/parser/parser.cpp
@@ -3,20 +3,44 @@
 
 static char parseArray[4][60];
 
+void eraseParserArray(){
+    memset(parseArray[0], 0x00, 60);
+    memset(parseArray[1], 0x00, 60);
+    memset(parseArray[2], 0x00, 60);
+    memset(parseArray[3], 0x00, 60);
+}
+
 void parserParse(char input[60]){
         sscanf(input, "%s%s%s%s", parseArray[0], parseArray[1], parseArray[2], parseArray[3]);
+
         switch(parserGetCmdId(parseArray[0])){
             case CMD_NOT_FOUND:
                 displayPrintf("%s :command not found :(\n", parseArray[0]);
+                eraseParserArray();
                 break;
             case CMD_INFO:
                 displayPrintf("Author: Ponyxis\n");
                 displayPrintf("Version: %s\n", VERSION);
                 displayPrintf("Platform: Teensy 4.0\n");
                 displayPrintf(("USB Host support now!\n"));
+                eraseParserArray();
                 break;
             case CMD_HELP:
                 displayPrintf("help screen work :)\n");
+                eraseParserArray();
+                break;
+            case CMD_BEEP:
+                int freq = 0;
+                int dur = 0;
+                sscanf(parseArray[1], "%d", &freq);
+                sscanf(parseArray[2], "%d", &dur);
+                if(freq > 0 && dur > 0){
+                    tone(2, freq, dur);
+                }else{
+                    displayPrintf("please enter freq or dur bigger than zero\n");
+                }
+
+                eraseParserArray();
                 break;
         }
 }
@@ -24,5 +48,6 @@ void parserParse(char input[60]){
 int parserGetCmdId(char input[20]){
     if(!strcmp("info", input)) return CMD_INFO;
     if(!strcmp("help", input)) return CMD_HELP;
+    if(!strcmp("beep", input)) return CMD_BEEP;
     return CMD_NOT_FOUND;
 }
--- lib/parser/parser.h
+++ lib/parser/parser.h
@@ -5,6 +5,10 @@
 #define CMD_NOT_FOUND 0
 #define CMD_INFO 1
 #define CMD_HELP 2
+#define CMD_BEEP 3
 
 extern void parserParse(char input[60]);
-extern int parserGetCmdId(char input[20]);
+
+void eraseParserArray();
+
+int parserGetCmdId(char input[20]);
